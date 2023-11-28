a5ea0de631b3bd7678159fbaece633e0f404475f
Saurabh7470 <SaurabhSen785@gmail.com>
2023-11-28 15:12:09 +0530

    Appointment & Visitor code changes

---
+++ Shared/Models/FileUpload_Image.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMS_App.Shared.Models
+{
+    public class SaveFile
+    {
+        public SaveFile()
+        {
+            Files = new List<FileData>();
+        }
+        public List<FileData> Files { get; set; }
+    }
+
+    public class FileData
+    {
+        public byte[] ImageBytes { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public long FileSize { get; set; }
+    }
+}
